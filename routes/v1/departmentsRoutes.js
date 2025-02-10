76d4ad6077db81fdc703a94e289675c679711558
Partha <github@partha51613.com>
2025-02-10 12:16:40 +0530

    departmentRoutes created

---
+++ routes/v1/departmentsRoutes.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const db = require("../../config/db");
+
+// Create a Router for /departments
+const router = express.Router();
+
+/**
+ * @route   GET /departments
+ * @desc    Get all departments
+ * @access  Public
+ */
+
+router.get("/", async (req, res) => {
+  try {
+    const departments = await db.query("SELECT * FROM departments");
+    res.json(departments);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: err.message });
+  }
+});
+
+/**
+ * @route   GET /departments/:id
+ * @desc    Get a department by ID
+ * @access  Public
+ */
+
+router.get("/:id", async (req, res) => {
+  try {
+    const [department] = await db.query(
+      "SELECT * FROM departments WHERE id = ?",
+      [req.params.id]
+    ); // Pass as an array to avoid SQL injection
+    if (!department) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(department); // Return a single object instead of an array
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: err.message });
+  }
+});
+
+/**
+ * @route   POST /departments
+ * @desc    Create a new department
+ * @access  Public
+ */
+
+router.post("/", async (req, res) => {
+  const { name, email, phone, type, isActive, createdAt, updatedAt } = req.body;
+
+  if (!name || !email || !phone || !type) {
+    return res
+      .status(400)
+      .json({ error: "One of the necessary field is missing" });
+  }
+
+  try {
+    const result = await db.query(
+      "INSERT INTO departments (name,email,phone,type) VALUES (?,?,?,?)",
+      [name, email, phone, type]
+    );
+    // `result.affectedRows` contains the count of updated rows
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ error: "Department not found or no changes made" });
+    }
+    res.json({
+      id: result.insertId,
+      message: "Department created successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+/**
+ * @route   PUT /departments/:id
+ * @desc    Update a department
+ * @access  Public
+ */
+router.put("/:id", async (req, res) => {
+  const { name, email, phone, type } = req.body;
+  try {
+    const result = await db.query(
+      "UPDATE departments SET name=?, email=?, phone=?, type=? WHERE id = ?",
+      [name, email, phone, type, req.params.id]
+    );
+
+    // `result.affectedRows` contains the count of updated rows
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ error: "Department not found or no changes made" });
+    }
+    res.json({ message: "Department updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+/**
+ * @route   DELETE /departments/:id
+ * @desc    Delete a department
+ * @access  Public
+ */
+router.delete('/:id', async (req, res) => {
+    try {
+        const result = await db.query('DELETE FROM departments WHERE id = ?', [req.params.id]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: 'Department not found' });
+        }
+        res.json({ message: 'Department deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = router;
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,9 +5,13 @@ const router = express.Router();
 * All v1 routes must be defined in this file. Then this bundle is imported into server.js
 */
 
-//User Route
+//User Routes
 const usersRoutes = require('./usersRoutes'); // Import users API
 router.use('/users', usersRoutes);   // /api/v1/users
+
+//Department Routes
+const departmentsRoutes = require("./departmentsRoutes")
+router.use('/departments', departmentsRoutes);   // /api/v1/departments
 
 /*
 * TO DO 
